ad7ad3744755631d5de3512cb47fe60f003850f9
Zafir <zafirhasan2006@gmail.com>
2022-08-13 17:54:59 +0400

    Add operator functionality
    The operators work when = is used. Only worked on it a bit so it works
    just for the first equation.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let firstNumber = "";
+let secondNumber = "";
+let currentOp = "";
+
 function add(x, y) {
     return x + y;
 }
@@ -14,3 +18,63 @@ function divide(x, y) {
     return x / y;
 }
 
+const operators = {
+    "+": add,
+    "-": subtract,
+    "×": multiply,
+    "÷": divide,
+};
+function operate(operator, x, y) {
+    if (operator == "" || x == "" || y == "")
+        return;
+    x = Number(x);
+    y = Number(y);
+    let result = operators[operator](x, y);
+    let screen = document.querySelector(".screen");
+    screen.textContent = result;
+}
+
+function displayOnScreen(numOp) {
+    screen = document.querySelector(".screen");
+    if ("+-÷×".includes(numOp)) {
+        if (secondNumber == "") {
+            currentOp = numOp;
+            screen.textContent = firstNumber + numOp;
+        } else {
+            
+        }
+    } else {
+        if (currentOp == "") {
+            firstNumber += numOp;
+            screen.textContent = firstNumber;
+        } else {
+            secondNumber += numOp;
+            screen.textContent = firstNumber + currentOp + secondNumber;
+        }
+    }
+    /*chcek if op
+    then check if there is a second number
+    if there is do the current operation on the first number and second number and add it to the screen
+    if not then set op to the current op and add it to the screen
+    if not op
+    check if currentOp
+    if it is then add it to that
+    if not add it to the first number
+    */
+}
+
+displayOnScreen("0");
+
+function linkButtons() {
+    allNumOps = document.querySelectorAll(".row button");
+    allNumOps.forEach(numOp => {
+        if (numOp.textContent == "=") {
+            numOp.addEventListener('click', () => {operate(currentOp, firstNumber, secondNumber);});
+        }
+        else {
+            numOp.addEventListener('click', (e) => {displayOnScreen(e.target.textContent);});
+        }
+    });
+}
+
+linkButtons();
